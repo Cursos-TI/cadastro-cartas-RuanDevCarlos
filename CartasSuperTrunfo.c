de7c26104d9ab1701e89212551f6153d35e60caf
ruandevcarlos <ruan.dev.carlos@gmail.com>
2025-10-28 23:37:01 -0300

    criação das variaveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,6 +6,9 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  char estado01, estado02, codigo01[5], codigo02[5], cidade01[10], cidade02[10];
+  int populacao01, populacao02, turismo01, turismo02;
+  float area01, area02, pib01, pib02;
 
   // Área para entrada de dados
 
